03f5c304d2d78de18dfbb7074c929565b82203c5
Chris James <quii@hey.com>
2022-07-27 19:56:24 +0100

    use the signals suggested by Pedro A

--- signal.go
+++ signal.go
@@ -3,10 +3,17 @@ package gracefulshutdown
 import (
 	"os"
 	"os/signal"
+	"syscall"
+)
+
+var (
+	signalsToListenTo = []os.Signal{
+		syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM,
+	}
 )
 
 func NewInterruptSignalChannel() <-chan os.Signal {
 	osSignal := make(chan os.Signal, 1)
-	signal.Notify(osSignal, os.Interrupt)
+	signal.Notify(osSignal, signalsToListenTo...)
 	return osSignal
 }
