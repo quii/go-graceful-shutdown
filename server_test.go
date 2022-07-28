81c4e21a21845565b3a04c5060b436090cfffd2f
Chris James <quii@hey.com>
2022-07-28 08:12:36 +0100

    test shutdown error prop

--- server_test.go
+++ server_test.go
@@ -7,13 +7,17 @@ import (
 	"github.com/quii/graceful-shutdown/assert"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestGracefulShutdownServer_Listen(t *testing.T) {
 	t.Run("happy path, listen, wait for interrupt, shutdown gracefully", func(t *testing.T) {
+		var (
+			interrupt = make(chan os.Signal)
+			spyServer = gracefulshutdown.NewSpyServer()
+			server    = gracefulshutdown.NewServer(interrupt, spyServer)
+		)
 
-		interrupt := make(chan os.Signal)
-		spyServer := gracefulshutdown.NewSpyServer()
 		spyServer.ListenAndServeFunc = func() error {
 			return nil
 		}
@@ -21,7 +25,6 @@ func TestGracefulShutdownServer_Listen(t *testing.T) {
 			return nil
 		}
 
-		server := gracefulshutdown.NewServer(interrupt, spyServer)
 		go server.Listen(context.Background())
 
 		// verify we call listen on the delegate server
@@ -33,18 +36,50 @@ func TestGracefulShutdownServer_Listen(t *testing.T) {
 	})
 
 	t.Run("when listen fails, return error", func(t *testing.T) {
-		interrupt := make(chan os.Signal)
-		spyServer := gracefulshutdown.NewSpyServer()
-		err := errors.New("oh no")
+		var (
+			interrupt = make(chan os.Signal)
+			spyServer = gracefulshutdown.NewSpyServer()
+			server    = gracefulshutdown.NewServer(interrupt, spyServer)
+			err       = errors.New("oh no")
+		)
+
 		spyServer.ListenAndServeFunc = func() error {
 			return err
 		}
-		server := gracefulshutdown.NewServer(interrupt, spyServer)
+
 		gotErr := server.Listen(context.Background())
 
 		assert.Equal(t, gotErr.Error(), err.Error())
 	})
-	
-	t.Skip("shutdown error gets propagated")
+
+	t.Run("shutdown error gets propagated", func(t *testing.T) {
+		var (
+			interrupt = make(chan os.Signal)
+			errChan   = make(chan error)
+			spyServer = gracefulshutdown.NewSpyServer()
+			server    = gracefulshutdown.NewServer(interrupt, spyServer)
+			err       = errors.New("oh no")
+		)
+
+		spyServer.ListenAndServeFunc = func() error {
+			return nil
+		}
+		spyServer.ShutdownFunc = func() error {
+			return err
+		}
+
+		go func() {
+			errChan <- server.Listen(context.Background())
+		}()
+
+		interrupt <- os.Interrupt
+
+		select {
+		case gotErr := <-errChan:
+			assert.Equal(t, gotErr.Error(), err.Error())
+		case <-time.After(500 * time.Millisecond):
+			t.Error("timed out waiting for shutdown error to be propagated")
+		}
+	})
 
 }
